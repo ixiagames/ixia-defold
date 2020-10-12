5cc79aea8a81c8ff6fbd93d6a7b9c1eeff8c8f73
melonin <hazagames404@gmail.com>
2020-10-12 13:40:27 +0700

    Various changes and fixes on Timeline

--- source/ixia/mgui/Timeline.hx
+++ source/ixia/mgui/Timeline.hx
@@ -2,55 +2,35 @@ package ixia.mgui;
 
 import haxe.ds.ReadOnlyArray;
 
-@:access(ixia.mgui.SubTimeline)
-class Timeline<T:Int> {
+@:access(ixia.mgui.TimelineTarget)
+class Timeline<T:TimelineTarget> {
 
     public var time(default, null):Float = 0;
-    public var timeScale:Float = 1;
-    public var children(get, never):ReadOnlyArray<SubTimeline<T>>;
-    var _children:Array<SubTimeline<T>> = [];
-    var _childrenMap:Map<T, SubTimeline<T>> = [];
+    public var timeScale #if debug (default, set) #end :Float = 1;
+    public var targets(get, never):ReadOnlyArray<T>;
+    var _targets:Array<T> = [];
     
     public function new() {}
-
-    public function add(id:T, startTime:Float, duration:Float):Void {
-        #if debug
-        if (_childrenMap.exists(id))
-            Error.error('$id already exists.');
-        if (startTime < 0)
-            Error.error('startTime cannot be smaller than 0. Got: $startTime');
-        if (duration <= 0)
-            Error.error('duration has to be positive. Got: $duration');
-        #end
-
-        var child = new SubTimeline<T>(id, startTime, duration);
-        _children.push(child);
-        _childrenMap[id] = child;
-    }
-
-    public inline function getChild(id:T):SubTimeline<T> {
-        return _childrenMap[id];
-    }
-
-    public function forward(delta:Float, ?onProgress:SubTimeline<T>->Void):Timeline<T> {
+    
+    public function forward(delta:Float, ?onProgress:T->Void):Timeline<T> {
         #if debug
         if (delta < 0)
             Error.error('delta cannot be negative. Got: $delta');
         #end
         
         time += delta * timeScale;
-        for (child in children) {
-            if (child.startTime >= time && child.progress < 1) {
-                child.time = time - child.startTime;
-                if (child.time < child.duration) {
-                    child.progress = child.time / child.duration;
+        for (target in targets) {
+            if (target.startTime <= time && target.progress < 1) {
+                target.time = time - target.startTime;
+                if (target.time < target.duration) {
+                    target.progress = target.time / target.duration;
                     if (onProgress != null)
-                        onProgress(child);
+                        onProgress(target);
                 } else {
-                    child.progress = 1;
-                    child.time = child.duration;
+                    target.progress = 1;
+                    target.time = target.duration;
                     if (onProgress != null)
-                        onProgress(child);
+                        onProgress(target);
                 }
             }
         }
@@ -59,42 +39,67 @@ class Timeline<T:Int> {
 
     public function reset():Void {
         time = 0;
-        for (child in _children)
-            child.time = child.progress = 0;
+        for (target in _targets)
+            target.time = target.progress = 0;
     }
 
-    inline function get_children():ReadOnlyArray<SubTimeline<T>> {
-        return _children;
+    inline function get_targets():ReadOnlyArray<T> {
+        return _targets;
     }
+
+    #if debug
+    inline function set_timeScale(value:Float):Float {
+        if (value < 0)
+            Error.error('timeScale cannot be negative. Got: $value');
+        return timeScale = value;
+    }
+    #end
 
 }
 
-class SubTimeline<T> {
+class TimelineTarget {
 
-    public var id(default, null):T;
+    public var timeline(default, null):Timeline<TimelineTarget>;
     public var duration(get, set):Float; var _duration:Float;
     public var startTime(get, set):Float; var _startTime:Float;
     public var time(default, null):Float = 0;
     public var progress(default, null):Float = 0;
     
-    public function new(id:T, startTime:Float, duration:Float) {
-        this.id = id;
+    public function new(timeline:Timeline<TimelineTarget>, startTime:Float, duration:Float) {
+        #if debug
+        if (startTime < 0)
+            Error.error('startTime cannot be smaller than 0. Got: $startTime');
+        if (duration <= 0)
+            Error.error('duration has to be positive. Got: $duration');
+        #end
+
+        this.timeline = timeline;
+        @:privateAccess timeline._targets.push(this);
+
         _startTime = startTime;
         _duration = duration;
     }
 
     inline function get_duration() return _duration;
     function set_duration(value:Float):Float {
-        progress = Math.max(time / value, 1);
+        #if debug
+        if (duration <= 0)
+            Error.error('duration has to be positive. Got: $duration');
+        #end
+
+        progress = Math.min(time / value, 1);
         return _duration = value;
     }
 
     inline function get_startTime() return _startTime;
     function set_startTime(value:Float):Float {
-        time += _startTime - value;
-        if (time < 0)
-            time = 0;
-        progress = Math.max(time / _duration, 1);
+        #if debug
+        if (startTime < 0)
+            Error.error('startTime cannot be smaller than 0. Got: $startTime');
+        #end
+
+        time = timeline.time > value ? timeline.time - value : 0;
+        progress = Math.min(time / _duration, 1);
         return _startTime = value;
     }
 
