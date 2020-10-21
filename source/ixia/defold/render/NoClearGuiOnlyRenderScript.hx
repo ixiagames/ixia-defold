a0c79ff48ce8e238a73511d60d593bcdc975eedc
melonin <hazagames404@gmail.com>
2020-10-21 17:24:48 +0700

    In NoClearGuiOnlyRenderScript, update projection every frame

--- source/ixia/defold/render/NoClearGuiOnlyRenderScript.hx
+++ source/ixia/defold/render/NoClearGuiOnlyRenderScript.hx
@@ -6,31 +6,33 @@ import defold.Vmath;
 import lua.Table;
 
 /**
- * Only render GUI without clearing.
+ * Only renders GUI without clearing.
  */
 class NoClearGuiOnlyRenderScript<T:{}> extends defold.support.RenderScript<T> {
 
     public var guiPred(default, null):RenderPredicate;
     public var textPred(default, null):RenderPredicate;
     public var view:Matrix4;
-    public var projection:Matrix4;
 
     override function init(self:T) {
         guiPred = Render.predicate(Table.create([ "gui" ]));
         textPred = Render.predicate(Table.create([ "text" ]));
         view = Vmath.matrix4();
-        projection = Vmath.matrix4_orthographic(0, Render.get_window_width(), 0, Render.get_window_height(), -1, 1);
     }
     
     override function update(self:T, dt:Float) {    
         Render.set_view(view);
-        Render.set_projection(projection);
+        Render.set_projection(getProjection());
         Render.enable_state(STATE_BLEND);
         Render.set_blend_func(BLEND_SRC_ALPHA, BLEND_ONE_MINUS_SRC_ALPHA);
         Render.enable_state(STATE_STENCIL_TEST);
         Render.draw(guiPred);
         Render.draw(textPred);
         Render.disable_state(STATE_STENCIL_TEST);
+    }
+
+    function getProjection():Matrix4 {
+        return Vmath.matrix4_orthographic(0, Render.get_window_width(), 0, Render.get_window_height(), -1, 1);
     }
 
 }
