f3d6c03bd27ecdca9fc13f4e0c6dafc2f4623652
melonin <hazagames404@gmail.com>
2020-10-23 21:21:18 +0700

    Fix OneOrMany

--- source/ixia/ds/OneOrMany.hx
+++ source/ixia/ds/OneOrMany.hx
@@ -1,4 +1,30 @@
 package ixia.ds;
 
+import haxe.ds.Either;
+
 @:forward
-abstract OneOrMany<T>(OneOfTwo<T, Array<T>>) from OneOfTwo<T, Array<T>> to OneOfTwo<T, Array<T>> {}
+abstract OneOrMany<T>(Either<T, Array<T>>) from Either<T, Array<T>> to Either<T, Array<T>> {
+
+    @:from inline static function fromOne<T>(one:T):OneOrMany<T> {
+        return Left(one);
+    }
+
+    @:from inline static function fromMany<T>(many:Array<T>):OneOrMany<T> {
+        return Right(many);  
+    }
+
+    @:to inline function toOne():Null<T> {
+        return switch(this) {
+            case Left(one): one; 
+            case _: null;
+        }
+    }
+
+    @:to inline function toMany():Null<Array<T>> {
+        return switch(this) {
+            case Right(many): many;
+            case _: null;
+        }
+    }
+
+}
