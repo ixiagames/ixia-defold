f3d4c675372806dc6175024495adc421c7a11549
melonin <hazagames404@gmail.com>
2020-10-23 10:49:38 +0700

    Add Rectangle set(), setXY() & setWH()

--- source/ixia/defold/types/Rectangle.hx
+++ source/ixia/defold/types/Rectangle.hx
@@ -10,6 +10,21 @@ abstract Rectangle(Vector4) {
         this = Vmath.vector4(x, y, w, h);
     }
 
+    public inline function set(x:Float, y:Float, w:Float, h:Float):Void {
+        setXY(x, y);
+        setWH(w, h);
+    }
+
+    public inline function setXY(x:Float, y:Float):Void {
+        this.x = x;
+        this.y = y;
+    }
+
+    public inline function setWH(w:Float, h:Float):Void {
+        this.z = w;
+        this.w = h;
+    }
+
     public var w(get, never):Float;
     inline function get_w() return this.z;
 
