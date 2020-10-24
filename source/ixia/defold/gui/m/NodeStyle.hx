6cb615cb41029164a42e7ab623f5eb45684ef94f
melonin <hazagames404@gmail.com>
2020-10-24 19:06:34 +0700

    Fix NodeStyle 'nodes' when set as string map

--- source/ixia/defold/gui/m/NodeStyle.hx
+++ source/ixia/defold/gui/m/NodeStyle.hx
@@ -1,11 +1,22 @@
 package ixia.defold.gui.m;
 
+import defold.types.Hash;
 import defold.types.HashOrString;
+using Defold;
 
 typedef NodeStyle = {
 
     ?enabled:Bool,
     ?flipbook:HashOrString,
-    ?nodes:Map<HashOrString, NodeStyle>
+    ?nodes:NodeStyleMap
 
+}
+
+@:forward
+abstract NodeStyleMap(Map<Hash, NodeStyle>) from Map<Hash, NodeStyle> {
+
+    @:from static inline function fromStringMap(map:Map<String, NodeStyle>):NodeStyleMap {
+        return [ for (key => style in map) key.hash() => style ];
+    }
+    
 }
