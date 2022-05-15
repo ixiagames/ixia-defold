a487e485a4bfdaba7923c8de1871e877e78dea31
melonin <hazagames404@gmail.com>
2022-05-15 20:37:41 +0700

    Add fromMessage casting to ixia.defold.types.Hash

--- source/ixia/defold/types/Hash.hx
+++ source/ixia/defold/types/Hash.hx
@@ -1,11 +1,16 @@
 package ixia.defold.types;
 
 import defold.types.HashOrString;
+import defold.types.Message;
 
 abstract Hash(defold.types.Hash) from defold.types.Hash to defold.types.Hash {
 
-    @:from public static inline function fromString(s:String):Hash {
-        return Defold.hash(s);
+    @:from public static inline function fromString(string:String):Hash {
+        return Defold.hash(string);
+    }
+
+    @:from public static inline function fromMessage(message:Message<Dynamic>):Hash {
+        return cast message;
     }
 
     @:to public inline function toHashOrString():HashOrString {
