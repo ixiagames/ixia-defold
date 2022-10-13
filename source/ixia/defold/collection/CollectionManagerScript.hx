2d0a508fa5ede51384c038dc7f9af93949b4d3eb
Sinh Nguyen <hazagames404@gmail.com>
2022-10-13 18:14:19 +0700

    Remove the debug traces from CollectionManagerScript

--- source/ixia/defold/collection/CollectionManagerScript.hx
+++ source/ixia/defold/collection/CollectionManagerScript.hx
@@ -49,9 +49,6 @@ class CollectionManagerScript<T:CollectionManagerScriptData> extends ExtScript<T
     }
 
     @post function loadCollection(proxyUrl:Url, async:Bool = false):Void {
-        #if debug
-        trace("Request to load: " + proxyUrl.fragment);
-        #end
         for (collection in collections) {
             if (collection.proxyUrl == proxyUrl && collection.loaded)
                 return;
@@ -67,23 +64,14 @@ class CollectionManagerScript<T:CollectionManagerScriptData> extends ExtScript<T
     }
 
     @post function unloadCollection(proxyUrl:Url):Void {
-        #if debug
-        trace("Request to unload: " + proxyUrl.fragment);
-        #end
         Msg.post(proxyUrl, CollectionproxyMessages.unload);
     }
 
     @post function enableCollection(proxyUrl:Url):Void {
-        #if debug
-            trace("Request to enable: " + proxyUrl.fragment);
-            #end
-            Msg.post(proxyUrl, CollectionproxyMessages.enable);
+        Msg.post(proxyUrl, CollectionproxyMessages.enable);
     }
 
     @post function disableCollection(proxyUrl:Url):Void {
-        #if debug
-        trace("Request to disable: " + proxyUrl.fragment);
-        #end
         Msg.post(proxyUrl, CollectionproxyMessages.disable);
     }
 
@@ -92,20 +80,12 @@ class CollectionManagerScript<T:CollectionManagerScriptData> extends ExtScript<T
         
         switch (message_id) {
             case CollectionproxyMessages.proxy_loaded:
-                #if debug
-                trace("Proxy loaded: " + sender.fragment);
-                #end
-                
                 var collection = get(sender);
                 collection.loaded = true;
                 if (collection.onLoaded != null)
                     collection.onLoaded(collection);
 
             case CollectionproxyMessages.proxy_unloaded:
-                #if debug
-                trace("Proxy unloaded: " + sender.fragment);
-                #end
-
                 var collection = get(sender);
                 collection.loaded = false;
                 if (collection.onUnloaded != null)
