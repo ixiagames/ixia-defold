8449ab520f6ac1d1ce978ba25da96f4a6aa6f7ea
Sinh Nguyen <hazagames404@gmail.com>
2023-02-13 17:16:27 +0700

    Add Loader.loadImage & update Loader.loadGuiTexture

--- source/ixia/defold/utils/Loader.hx
+++ source/ixia/defold/utils/Loader.hx
@@ -6,27 +6,35 @@ import defold.Image;
 
 class Loader {
 
-    public static function loadGuiTexture(url:String, ?textureId:String, callback:(?textureId:String, ?error:String)->Void):Void {
-        if (textureId == null)
-            textureId = url;
+    public static function loadImage(url:String, callback:(?image:ImageLoadResult, ?error:String)->Void):Void {
         Http.request(url, "GET", (_, id, res) -> {
             if (res.status == 302 || res.status == 301) {
-                loadGuiTexture(res.headers["location"], textureId, callback);
+                loadImage(res.headers["location"], callback);
 
             } else if (res.status == 200 || res.status == 304) {
                 var image = Image.load(res.response);
-                if (image == null) {
-                    Error.error('Unable to load $url.');
-                    return;
-                }
+                if (image != null)
+                    callback(image);
+                else
+                    callback(null, 'Unable to load $url.');
+            } else
+                callback(null, 'Unable to get image: ${res.response}.');
+        });
+    }
+
+    public static function loadGuiTexture(url:String, ?textureId:String, callback:(?textureId:String, ?error:String)->Void):Void {
+        if (textureId == null)
+            textureId = url;
+        loadImage(url, (?image, ?error) -> {
+            if (error != null)
+                callback(textureId, error);
+            else {
                 var result = Gui.new_texture(textureId, image.width, image.height, image.type, image.buffer);
                 if (result.success)
                     callback(textureId);
                 else
-                    callback(null, 'Gui.new_texute error code ${result.code}.');
-
-            } else
-                callback(null, 'Unable to get image: ${res.response}.');
+                    callback(textureId, 'Gui.new_texute error code ${result.code}.');
+            }
         });
     }
     
